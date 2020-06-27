b88a9c6a7ec561a63006705204d0c7823386f3f4
aplayton6 <aplayton6@icloud.com>
2020-06-27 14:59:50 -0400

    Made API call to meme source

--- MemeGenerator.js
+++ MemeGenerator.js
@@ -3,7 +3,22 @@ import React, {Component} from "react"
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state ={}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+    }
+    
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            })
     }
     
     render() {
