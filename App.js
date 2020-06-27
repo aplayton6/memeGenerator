1687e700eaad9cd8d7a7f112757a91c0bc968e7a
aplayton6 <aplayton6@icloud.com>
2020-06-27 14:48:39 -0400

    Added header and meme generator components

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
 
 function App() {
-	return(
-		<h1>Hello world!</h1>
-	)
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
 }
 
-export default App;
+export default App
